17b8f5a3738055a50efc9d0141df152d5cdac1d5
SlavinaKlisarska46011 <46001z@unibit.com>
2021-06-30 22:47:12 +0300

    Updates

--- src/main/java/com/bookLords/model/smartRecommendation/SelectRecommender.java
+++ src/main/java/com/bookLords/model/smartRecommendation/SelectRecommender.java
@@ -2,7 +2,10 @@ package com.bookLords.model.smartRecommendation;
 
 import com.bookLords.model.Book;
 import com.bookLords.model.Bookshelf;
+import com.bookLords.model.Rating;
 import com.bookLords.model.User;
+import com.bookLords.model.daos.BookDBDAO;
+import com.bookLords.model.exceptions.BookException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -18,14 +21,28 @@ public class SelectRecommender {
     @Autowired
     ContentBasedRecommender contentBasedRecommender;
 
+    @Autowired
+    BookDBDAO bookDBDAO;
+
     public Recommender selectRecommender(User user) {
         Set<Bookshelf> userBookshelves = user.getBookshelves();
+        Map<Integer, Rating> userRatings = user.getRatings();
         Set<Book> userBooks = new HashSet<>();
         if (!userBookshelves.isEmpty()) {
             for (Bookshelf bookshelf : userBookshelves) {
                 userBooks.addAll(bookshelf.getBooks().keySet());
             }
         }
+
+        if (!userRatings.isEmpty()) {
+            for (int bookId : userRatings.keySet()){
+                try {
+                    userBooks.add(bookDBDAO.getBookByID(bookId));
+                } catch (BookException e) {
+                }
+            }
+        }
+
         return !userBooks.isEmpty() ? contentBasedRecommender : slopeOneRecommender;
     }
 }
